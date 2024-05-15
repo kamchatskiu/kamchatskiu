f0dc0fff167af382f9db18675bc757a100a5d72c
kamchatskiu <motelev99@gmail.com>
2024-05-15 22:51:18 +1200

    Create java.js

---
+++ java.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $('.sidebar').append('<h2>Навигация</h2><ul><li><a href="#">О нас</a></li><li><a href="#">Услуги</a></li><li><a href="#">Контакты</a></li></ul>');
+});
+
+// jQuery
+$('.sidebar').append('<h2>Навигация</h2><ul><li><a href="#">О нас</a></li><li><a href="#">Услуги</a></li><li><a href="#">Контакты</a></li></ul>');
